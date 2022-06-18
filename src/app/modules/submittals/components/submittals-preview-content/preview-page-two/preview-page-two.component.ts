6e7447c20973377b3c4a894cb7be9c1620c19fe5
Himanshu Pandey <himanshu.p89@gmail.com>
2022-06-19 05:00:22 +0530

    preview two

---
+++ src/app/modules/submittals/components/submittals-preview-content/preview-page-two/preview-page-two.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-preview-page-two',
+  templateUrl: './preview-page-two.component.html',
+  styleUrls: ['./preview-page-two.component.scss']
+})
+export class PreviewPageTwoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
