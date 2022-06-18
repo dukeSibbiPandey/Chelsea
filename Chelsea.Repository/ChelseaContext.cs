94816fa78f6b04a3cfcd6cc26f0a32fbc759e9a8
pramodclinch <pramod.kumar@clinchinfosystems.com>
2022-06-18 21:06:43 +0530

    commit changes

---
+++ Chelsea.Repository/ChelseaContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Chelsea.Repository
+{
+    public class ChelseaContext: DbContext
+    {
+        public ChelseaContext()
+        {
+        }
+
+        public ChelseaContext(DbContextOptions<ChelseaContext> options)
+            : base(options)
+        {
+        }
+
+        //public virtual DbSet<Results> Results { get; set; }
+    }
+    
+}
