1fc9a3b531b7c03b7f0f23158c94aa3f459987a2
Himanshu Pandey <himanshu.p89@gmail.com>
2022-06-21 18:13:11 +0530

    form

--- ChelseaApp/ClientApp/src/app/modules/submittals/components/submittals-detail-form/submittals-detail-form.component.ts
+++ ChelseaApp/ClientApp/src/app/modules/submittals/components/submittals-detail-form/submittals-detail-form.component.ts
@@ -10,7 +10,7 @@ import { PrimeNGConfig } from 'primeng/api';
   providers: [PrimeNGConfig, MessageService]
 })
 export class SubmittalsDetailFormComponent implements OnInit {
-  registerForm: FormGroup;
+  submittalDetailForm: FormGroup;
   submitted = false;
   toastPosition = 'top-center';
 
@@ -22,21 +22,40 @@ export class SubmittalsDetailFormComponent implements OnInit {
     })
   }
   createForm = (callback: any): void => {
-    this.registerForm = this._FormBuilder.group(
+    this.submittalDetailForm = this._FormBuilder.group(
       {
-        types: ['', Validators.required],
-        mfg: ['', Validators.required],
-        part: ['', Validators.required],
-        description: ['', Validators.required],
         id: [0],
+        submittalDate: ['', Validators.required],
+        jobName: ['', Validators.required],
+        submittals: ['', Validators.required],
+        address: this._FormBuilder.group({
+          addressLine1: ['', [Validators.required]],
+          addressLine2: ['', [Validators.required]],
+          state: ['', [Validators.required]],
+          city: ['', [Validators.required]],
+          postalCode: [Validators.required],
+        }),
+        projectManager: this._FormBuilder.group({
+          name: ['', [Validators.required]],
+          phone: ['', [Validators.required]],
+          email: ['', [Validators.required]],
+        }),
+        contractor: this._FormBuilder.group({
+          name: ['', [Validators.required]],
+          addressLine1: ['', [Validators.required]],
+          addressLine2: ['', [Validators.required]],
+          state: ['', [Validators.required]],
+          city: ['', [Validators.required]],
+          postalCode: ['', [Validators.required]],
+        }),
       }
     )
     if (callback) {
       callback()
     }
   }
-  get formControl() { return this.registerForm.controls };
-  
+  get formControl() { return this.submittalDetailForm.controls };
+
   toastMsg(severity: any, summary: any, detail: any, life: any) {
     this.messageService.add({ key: 'headerFormToast', severity: severity, summary: summary, detail: detail, life: life, closable: true });
   }
@@ -44,7 +63,7 @@ export class SubmittalsDetailFormComponent implements OnInit {
 
   setFormValue = () => {
     const res: any = {};
-    // this.registerForm.controls['types'].setValue(res['types']);
+    // this.submittalDetailForm.controls['types'].setValue(res['types']);
   }
 
 }
