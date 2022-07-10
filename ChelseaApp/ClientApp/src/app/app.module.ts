1e3de73f93975ef9b0d05fdd88c8a6c23bf40aa9
Himanshu Pandey <hp.frontend.developer@gmail.com>
2022-07-11 01:37:46 +0530

    pef v2

--- ChelseaApp/ClientApp/src/app/app.module.ts
+++ ChelseaApp/ClientApp/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PdfEditorComponent } from './pdf-editor/pdf-editor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PdfEditorComponent
   ],
   imports: [
     BrowserModule
