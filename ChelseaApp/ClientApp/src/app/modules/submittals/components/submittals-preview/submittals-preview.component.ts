d729362fbc1cb12ebfa396b1730257bbb4660359
Himasnhu <hp.frontend.developer@gmail.com>
2022-08-01 07:22:23 +0530

    preview

--- ChelseaApp/ClientApp/src/app/modules/submittals/components/submittals-preview/submittals-preview.component.ts
+++ ChelseaApp/ClientApp/src/app/modules/submittals/components/submittals-preview/submittals-preview.component.ts
@@ -1,4 +1,11 @@
-import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter, ElementRef, ViewChild, AfterViewInit, } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MessageService, PrimeNGConfig } from 'primeng/api';
+import { HttpService } from 'src/app/components/http.service';
+import { SubmittalService } from '../../submittal.service';
+import WebViewer from '@pdftron/pdfjs-express';
+import { PdfHelperService } from '../../pdfhelper.service';
 const submittalItem: any = {
   name: 'F1',
   status: '',
@@ -14,21 +21,64 @@ const submittalItem: any = {
 @Component({
   selector: 'app-submittals-preview',
   templateUrl: './submittals-preview.component.html',
-  styleUrls: ['./submittals-preview.component.scss']
+  styleUrls: ['./submittals-preview.component.scss'],
+  providers: [PrimeNGConfig, MessageService]
 })
-export class SubmittalsPreviewComponent implements OnInit {
-  @Input() pdfActionConfig: any = {};
+export class SubmittalsPreviewComponent implements OnInit, AfterViewInit {
+  @ViewChild('viewer2', { static: false }) viewer1: ElementRef;
   @Output() previewSubmitCallback: EventEmitter<any> = new EventEmitter();
   saveDialogTitle = 'Save PDF';
   isDetailEditDialog = false;
   submittal: any = submittalItem;
-  constructor() { }
+  previewUrl: any;
+  wvInstance: any;
+  isFormSubmit = false;
+  id: any
+  dialogConfig: any = null;
+  icon: any = {
+    BACK_ICON: ''
+  }
+  constructor(private httpService: HttpService, private _SubmittalService: SubmittalService, public activatedRoute: ActivatedRoute, private router: Router, private sanitizer: DomSanitizer, private messageService: MessageService) { }
 
   ngOnInit(): void {
+    this.id = this.activatedRoute.snapshot.params['submittalId'];
+    const data = JSON.parse(localStorage.getItem('submittalObject'));
     this.submittal = {
-      ...this.pdfActionConfig.pdfFiles
+      ...data.pdfFiles
     }
+    this.dialogConfig = data;
+    this.wvDocumentLoadedHandler = this.wvDocumentLoadedHandler.bind(this);
+  }
 
+  ngAfterViewInit(): void {
+    this.previewUrl = this.dialogConfig.config.previewUrl;
+    WebViewer(
+      {
+        path: '../lib',
+        initialDoc: this.previewUrl,
+        licenseKey: 'irld89CMAcwPvMz4SJzz',
+      }, this.viewer1.nativeElement).then(instance => {
+        this.wvInstance = instance;
+        this.createHeader();
+        instance.setFitMode('FitWidth')
+        instance.disableElements(['header', 'leftPanel']);
+        instance.disableFeatures([instance.Feature.Print, instance.Feature.FilePicker]);
+        instance.openElements(['notesPanel']);
+        instance.docViewer.on('documentLoaded', this.wvDocumentLoadedHandler)
+      })
+  }
+
+  wvDocumentLoadedHandler(): void {
+    const { annotManager } = this.wvInstance;
+    let submitalData = this.dialogConfig.pdfFiles;
+    let xfdfData = submitalData.files.annotations;
+    if (xfdfData) {
+      annotManager.importAnnotations(xfdfData).then(importedAnnotations => { });
+    }
+  }
+  createHeader = async () => {
+    let blobDoc = await PdfHelperService.CreatePdfHeader(this.previewUrl, this.dialogConfig.pdfFiles);
+    this.wvInstance.loadDocument(blobDoc);
   }
   handleUpdateDetail = () => {
     let pdfFiles = {
@@ -41,13 +91,17 @@ export class SubmittalsPreviewComponent implements OnInit {
       runs: this.submittal.runs,
     }
     let config = {
-      ...this.pdfActionConfig.config
+      ...this.dialogConfig.config
     }
     let postDto = {
       pdfFiles: pdfFiles,
       config: config
     }
-    this.previewSubmitCallback.emit(postDto)
+    localStorage.removeItem('submittalObject');
+    localStorage.removeItem('updatedHeader');
+    localStorage.setItem('updatedHeader', JSON.stringify(postDto));
+    // this.previewSubmitCallback.emit(postDto)
+    this.router.navigate([`/submittals/form/${this.id}/step/2`]);
   }
   handleDetailEditDialog = (value: boolean) => {
     this.isDetailEditDialog = value
