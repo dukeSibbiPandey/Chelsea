7986956b05bfd7f75154f23d5726d8f1604738ea
Himasnhu <hp.frontend.developer@gmail.com>
2022-09-02 22:53:09 +0530

    duplicat index

--- ChelseaApp/ClientApp/src/app/modules/submittals/components/submittals-form-step2/submittals-form-step2.component.ts
+++ ChelseaApp/ClientApp/src/app/modules/submittals/components/submittals-form-step2/submittals-form-step2.component.ts
@@ -179,6 +179,8 @@ export class SubmittalsFormStep2Component implements OnInit {
     let item = JSON.parse(JSON.stringify(res.submittal));
     item.name = "Type " + (this.submittalsTpl.length + 1);
     item.files = [];
+    const nextIndex = this.submittalsTpl.length
+    this.openIndex.push(nextIndex.toString())
     this.submittalsTpl.push(item);
     this.updateOldState();
   }
